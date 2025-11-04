6053e6b470f5f54f4eb71e64833f89a3179cb078
Cris <111942487+Christjacoben@users.noreply.github.com>
2025-11-04 17:23:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -180,8 +180,8 @@ app.post("/api/login", (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 60 * 60 * 1000,
     });
 
